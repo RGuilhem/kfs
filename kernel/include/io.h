8fe2d8f9ddc8e2cecc186f35f8f8d7c61ecd9da8
Guilhem Raux <g.raux@icloud.com>
2023-01-20 14:23:35 +0100

    outb and inb to write de devices port

---
+++ kernel/include/io.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   io.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: graux <graux@student.42lausanne.ch>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/20 14:17:29 by graux             #+#    #+#             */
+/*   Updated: 2023/01/20 14:18:53 by graux            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef IO_H
+# define IO_H
+
+static inline void outb(uint16_t port, uint8_t val)
+{
+    asm volatile ( "outb %0, %1" : : "a"(val), "Nd"(port) );
+}
+
+static inline uint8_t inb(uint16_t port)
+{
+    uint8_t ret;
+    asm volatile ( "inb %1, %0"
+                   : "=a"(ret)
+                   : "Nd"(port) );
+    return ret;
+}
+
+static inline void io_wait(void)
+{
+    outb(0x80, 0);
+}
+
+#endif
