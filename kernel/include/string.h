07eb2d5a1989d58dc88c9da6cda8cf115c6c68da
Guilhem Raux <g.raux@icloud.com>
2023-01-20 14:44:24 +0100

    changed structure of project

--- kernel/include/string.h
+++ kernel/include/string.h
@@ -6,7 +6,7 @@
 /*   By: graux <graux@student.42lausanne.ch>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 23:28:13 by graux             #+#    #+#             */
-/*   Updated: 2023/01/19 23:55:57 by graux            ###   ########.fr       */
+/*   Updated: 2023/01/20 14:33:35 by graux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,9 @@
 # include "kernel.h"
 
 size_t	strlen(const char *str);
+int		memcmp(const void* aptr, const void* bptr, size_t size);
+void*	memset(void* bufptr, int value, size_t size);
+void*	memmove(void* dstptr, const void* srcptr, size_t size);
+void*	memcpy(void* restrict dstptr, const void* restrict srcptr, size_t size);
 
 #endif
